f0407c66c88fd3d8c68274cd259fba8022374f17
Zzmrhy <144816511+Zzmrhy@users.noreply.github.com>
2026-01-07 12:53:51 -0500

    Change print statements to reflect remote context

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 print("Git/Github Demo")
 
-print("This works out - local")
+print("This works out - remote")
 print("This is a decent day")
-print("This is an ok day - local")
+print("This is an ok day - remote")
 print("Greetings from local repository")
